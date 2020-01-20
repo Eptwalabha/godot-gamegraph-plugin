b1c5707eb7b8641c9d687aace25ee5f257b0884e
Eptwalabha <damiengabrielle@gmail.com>
2020-01-20 21:53:51 +0100

    GameGraph Node add warning on missing Resource

--- addons/game_graph/GameGraph.gd
+++ addons/game_graph/GameGraph.gd
@@ -4,3 +4,9 @@ extends Node
 class_name GameGraph
 
 export(Resource) var dialogs
+
+func _get_configuration_warning() -> String:
+	if dialogs is GameGraphResource:
+		return ""
+	else:
+		return "In order to work, this custom Node requires a GameGraphResource"
