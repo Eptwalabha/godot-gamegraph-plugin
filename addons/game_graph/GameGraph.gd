ddf059cda3e7ff7d029388201639e7d8df1454ff
Eptwalabha <damiengabrielle@gmail.com>
2020-01-27 23:56:54 +0100

    the dialog system works but also crashes at the end

--- addons/game_graph/GameGraph.gd
+++ addons/game_graph/GameGraph.gd
@@ -7,9 +7,9 @@ signal event_triggered(dialog_name, event_name)
 
 var dialogs = {}
 
-var i = 0
 var current_dialog = ''
 var current_node_id = 0
+var current_line_id = 0
 
 export(Resource) var story
 
@@ -40,42 +40,66 @@ func start_dialog(dialog_name) -> bool:
 	return false
 
 func next_dialog() -> Dictionary:
-	i += 1
-	if i == 1:
-		return {
-			'type': 'dialog',
-			'text': 'super mon text'
-		}
+	var current_node = get_current_node()
+	if current_node.type == 'start':
+		jump_to_next_node()
+		for event in current_node.events:
+			emit_signal("event_triggered", current_dialog, event)
+		return get_next_item()
 	else:
-		return {
-			'type': 'choice',
-			'text': 'super first choice',
-			'choices': [
-				{
-					'label': 'super',
-					'id': 0
-				}
-			]
-		}
+		return get_next_item()
 
-func pick_choice(choice_id: int) -> Dictionary:
-	if choice_id == 0:
+func jump_to_next_node() -> void:
+	current_node_id = get_current_node().output
+	current_line_id = 0
+
+func get_current_node() -> Dictionary:
+	return dialogs[current_dialog].nodes[current_node_id]
+
+func get_next_item() -> Dictionary:
+	var current_node = get_current_node()
+	if current_node.type == 'dialog':
+		if current_line_id >= len(current_node.lines):
+			for event in current_node.events:
+				emit_signal("event_triggered", current_dialog, event)
+			jump_to_next_node()
+			return get_next_item()
+		else:
+			var line = current_node.lines[current_line_id]
+			for event in line.events:
+				emit_signal("event_triggered", current_dialog, event)
+			current_line_id += 1
+			return {
+				'type': 'dialog',
+				'text': line.text
+			}
+	elif current_node.type == 'choice':
+		var choices = []
+		for i in range(len(current_node.choices)):
+			var choice = current_node.choices[i]
+			choices.push_back({
+				'text': choice.text,
+				'id': i
+			})
 		return {
 			'type': 'choice',
-			'text': 'again ?',
-			'choices': [
-				{
-					'label': 'yes',
-					'id': 0
-				},
-				{
-					'label': 'nope!',
-					'id': 1
-				}
-			]
+			'text': current_node.question,
+			'choices': choices
 		}
 	else:
 		return get_ending()
+
+func pick_choice(choice_id: int) -> Dictionary:
+	var current_node = get_current_node()
+	if current_node.type == 'choice':
+		if choice_id < len(current_node.choices):
+			var choice = current_node.choices[choice_id]
+			for event in choice.events:
+				emit_signal("event_triggered", [current_dialog, event])
+			current_node_id = choice.output
+			current_line_id = 0
+			return get_next_item()
+	return get_ending()
 
 func get_ending() -> Dictionary:
 	return { 'type': "end" }
@@ -93,7 +117,6 @@ func _build_automaton() -> void:
 			'label': dialog.label,
 			'nodes': connected_nodes
 		}
-	print(dialogs)
 
 func _build_nodes(node_resources: Array) -> Dictionary:
 	var nodes = {}
@@ -110,34 +133,71 @@ func _build_events(node_resources: Array) -> Dictionary:
 	return events
 
 func _build_connected_nodes(nodes: Dictionary, events: Dictionary, connections: Array) -> Dictionary:
-	var automata = {}
-#	TODO cycle through all connections, complete all node an build the graph
-	return automata
+	var final_nodes = {}
+	for c in connections:
+		var from = nodes.get(c[0])
+		var output_port = c[1]
+		var is_output_event = events.has(c[2])
+		var is_output_node = nodes.has(c[2])
+		if not nodes.has(c[0]) or not (is_output_event or is_output_node):
+			continue
+
+		if not final_nodes.has(c[0]):
+			final_nodes[c[0]] = from
+
+		if is_output_node:
+			var to = nodes.get(c[2])
+			if not final_nodes.has(c[2]):
+				final_nodes[c[2]] = to
+			if from.type == 'choice':
+				final_nodes[c[0]].choices[output_port].output = c[2]
+			else :
+				final_nodes[c[0]].output = c[2]
+		elif is_output_event:
+			var event_name : String = events.get(c[2])
+			if from.type == 'dialog':
+				if output_port == 0:
+					final_nodes[c[0]].events.push_back(event_name)
+				else:
+					final_nodes[c[0]].lines[output_port - 1].events.push_back(event_name)
+			elif from.type == 'start':
+				final_nodes[c[0]].events.push_back(event_name)
+			elif from.type == 'choice':
+				final_nodes[c[0]].choices[output_port].events.push_back(event_name)
+	return final_nodes
 
 func _to_dictionary(node) -> Dictionary:
 	if node is GameGraphNodeDialogResource:
 		var dict = {
 			'type': "dialog",
-			'lines': []
+			'lines': [],
+			'output': -1,
+			'events': []
 		}
 		for line in node.dialog_lines:
 			dict.lines.push_back({
 				'text': line.dialog_key,
 				'who': line.who,
-				'how': line.how
+				'how': line.how,
+				'events': []
 			})
 		return dict
 	elif node is GameGraphNodeChoiceResource:
 		var dict = {
 			'type': "choice",
 			'question': node.question,
-			'choices': []
+			'choices': [],
+			'output': -1,
+			'events': []
 		}
 		for choice in node.choices:
 			dict.choices.push_back({
-				'text': choice.choice_key
+				'text': choice.choice_key,
+				'events': []
 			})
 		return dict
 	return {
-		'type': "start"
+		'type': "start",
+		'output': -1,
+		'events': []
 	}
